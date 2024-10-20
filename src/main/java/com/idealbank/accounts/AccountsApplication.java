e23c213104219198e4a5f8e916961d9318070432
vivekkarli <vivekkarli7@gmail.com>
2024-10-20 22:10:32 +0530

    add fiegn client to connect loans & cards microservises

--- src/main/java/com/idealbank/accounts/AccountsApplication.java
+++ src/main/java/com/idealbank/accounts/AccountsApplication.java
@@ -10,11 +10,13 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@EnableFeignClients
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Accounts microservice REST API Documentation",
---
+++ src/main/java/com/idealbank/accounts/service/client/CradsFeignClient.java
@@ -0,0 +1,14 @@
+package com.idealbank.accounts.service.client;
+
+import com.idealbank.accounts.dto.CardsDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("cards")
+public interface CradsFeignClient {
+
+    @GetMapping("/api/fetch")
+    ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
+}
