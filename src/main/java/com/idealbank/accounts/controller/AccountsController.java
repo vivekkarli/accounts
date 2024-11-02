361615128d22b76fc5b60a44aa203f623fbea652
vivekkarli <vivekkarli7@gmail.com>
2024-11-02 21:18:19 +0530

    implement ratelimiter pattern

--- src/main/java/com/idealbank/accounts/controller/AccountsController.java
+++ src/main/java/com/idealbank/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.idealbank.accounts.dto.CustomerDto;
 import com.idealbank.accounts.dto.ErrorResponseDto;
 import com.idealbank.accounts.dto.ResponseDto;
 import com.idealbank.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -211,7 +212,7 @@ public class AccountsController {
                 .body(buildVersion);*/
     }
 
-    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+    private ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
         logger.info("getBuildInfoFallback() got invoked");
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -236,11 +237,18 @@ public class AccountsController {
             )
     }
     )
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(environment.getProperty("JAVA_HOME"));
+    }
+
+    private ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA 21"));
     }
 
     @Operation(
